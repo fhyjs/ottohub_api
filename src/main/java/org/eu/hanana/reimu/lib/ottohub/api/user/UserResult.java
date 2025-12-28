11f53eaea9c2b3577624a2d6fca7e77473208947
fhyjs <fenhyjs@163.com>
2025-12-28 11:49:23 +0800

    1.1.8

--- src/main/java/org/eu/hanana/reimu/lib/ottohub/api/user/UserResult.java
+++ src/main/java/org/eu/hanana/reimu/lib/ottohub/api/user/UserResult.java
@@ -3,6 +3,6 @@ package org.eu.hanana.reimu.lib.ottohub.api.user;
 import org.eu.hanana.reimu.lib.ottohub.api.ApiResultBase;
 
 public class UserResult extends ApiResultBase {
-    public int uid,experience,blog_num,video_num,followings_count,fans_count;
-    public String username,intro,time,avatar_url,sex,honour,cover_url;
+    public int uid,experience,blog_num,video_num,followings_count,fans_count,new_message_num;
+    public String username,intro,time,avatar_url,sex,honour,cover_url,last_time;
 }
